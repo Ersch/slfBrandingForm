402bbed4d4474c2904d7e1957a10dda3623885f7
Ersch <eric.schidlo@pm.me>
2024-11-16 00:13:06 +0100

    first commit

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import {InputTextModule} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [
+    ButtonModule,
+    CheckboxModule,
+    InputTextModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export default class HomeComponent {
 
+  private formBuilder = inject(FormBuilder);
+  protected sent = signal<boolean>(false);
+
+  protected form = this.formBuilder.group({
+    name: ['', Validators.required],
+    surname: ['', Validators.required],
+    function: [''],
+    company: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    services: this.formBuilder.array([
+      this.formBuilder.group({
+        visualIdentity: [false],
+        webDesign: [false],
+        socialMedia: [false],
+        packaging: [false],
+        audit: [false],
+        freelance: [false],
+      })
+    ]),
+    message: [''],
+    startDate: [''],
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form);
+    }
+  }
 }
